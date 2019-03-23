b59290c87e97e6c817847d99b0cbc4bd2ff54d4f
premruethai <pramrutai34@gmail.com>
2019-03-23 10:50:17 +0700

    ร้องขอข้อมูล JSON จาก Web API มาแสเงในหน้าเพจ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 import { PopularPage } from '../pages/popular/popular';
 import { SciencePage } from '../pages/science/science';
@@ -14,6 +12,7 @@ import { DetailPage } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -26,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
