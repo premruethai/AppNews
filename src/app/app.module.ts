8d08697b8d40765380e706f2fae0152efd7bffd9
premruethai <pramrutai34@gmail.com>
2019-03-23 09:55:24 +0700

    Import Page(3ขั้นตอน)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { PopularPage } from '../pages/popular/popular';
+import { SciencePage } from '../pages/science/science';
+import { MoviePage } from '../pages/movie/movie';
+import { DetailPage } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage,
     TabsPage
   ],
   imports: [
@@ -25,10 +29,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage,
     TabsPage
   ],
   providers: [
